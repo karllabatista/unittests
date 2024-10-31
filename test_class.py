4cf6eefb26edc59b20c756ba1171fda109396e0d
karllabatista <karllabatista19@gmail.com>
2024-10-31 16:28:11 -0400

    first commit

---
+++ test_class.py
@@ -0,0 +1,8 @@
+class TestClass:
+
+    def test_me(self):
+        assert True
+
+    def test_me2(self):
+        assert True
+    
---
+++ test_lab1.py
@@ -0,0 +1,8 @@
+def test_me():
+    assert True
+
+def test_me2():
+    assert True
+
+def test_me2():
+    assert False
